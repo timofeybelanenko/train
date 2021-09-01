65eeb6ecb1979d4872e2b932f1f3dc0ea4e97e6d
Tim <timcode@yahoo.com>
2021-09-01 23:44:38 +0300

    Задача 2 - добавил условия задания

--- venv/2/task2.py
+++ venv/2/task2.py
@@ -1,3 +1,5 @@
+# Задача 2. Дан файл, содержащий строку с цифрами от 0 до 9. Необходимо отсортировать значения по возрастанию и записать в другой файл.
+
 with open('u_r_txt_file.txt') as file:
     line = file.readline().replace('\n', '')
     letters = sorted([letter for letter in line])
